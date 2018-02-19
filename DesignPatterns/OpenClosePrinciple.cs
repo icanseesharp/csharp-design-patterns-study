750780814be7ca4e8271d2c5695924ce89ac6c1e
Sagar Yerva <sagar.yerva.dev@gmail.com>
2018-02-19 18:36:53 +0530

    Implemented SRP

---
+++ DesignPatterns/OpenClosePrinciple.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large, Yuge
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            Name = name ?? "";
+            Color = color;
+            Size = size;
+        }
+
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize (IEnumerable<Product> Products ,Size Size)
+        {
+            foreach(var product in Products)
+            {
+                if (product.Size == Size) yield return product;
+            }
+        }
+
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> Products, Color Color)
+        {
+            foreach (var product in Products)
+            {
+                if (product.Color == Color) yield return product;
+            }
+        }
+
+        public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> Products, Size Size, Color Color)
+        {
+            foreach (var product in Products)
+            {
+                if (product.Size == Size && product.Color == Color) yield return product;
+            }
+        }
+    }
+
+    public class OpenClosePrinciple
+    {
+        static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree  = new Product("Tree", Color.Green, Size.Medium);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            Console.WriteLine("Filter by Size Small : \n");
+            var smallProducts = pf.FilterBySize(products, Size.Small);
+            var greenProducts = pf.FilterByColor(products, Color.Green);
+
+            foreach(var p in pf.FilterByColor(products,Color.Green))
+            {
+                Console.WriteLine(string.Format("{0} is Green", p.Name));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatterns/SingleResponsibility.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class Journal
+    {
+        public readonly List<string> Entries = new List<string>();
+        public int Count = 0;
+        public int AddEntry(string entry)
+        {
+            Entries.Add(string.Format("{0}: {1}\n", ++Count, entry));
+            return Count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            Entries.RemoveAt(index);
+        }
+
+        //This method breaks Single Responsibility (SRP), to maintain SRP, we've implemented Persistence class
+        public void Save(Journal j, string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, Entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string fileName, bool overwrite = false)
+        {
+            if (!File.Exists(fileName) || overwrite)
+            {
+                File.WriteAllText(fileName, j.ToString());
+            }
+        }
+    }
+    public class SingleResponsibility
+    {
+        //public static void Main()
+        //{
+        //    Journal j = new Journal();
+        //    j.AddEntry("Today I worked on SRP");
+        //    j.AddEntry("Today I worked on UoW");
+
+        //    Persistence p = new Persistence();
+        //    var fileName = @"D:\temp.txt";
+
+        //    p.SaveToFile(j, fileName, true);
+        //    Process.Start(fileName);
+        //}
+    }
+}
